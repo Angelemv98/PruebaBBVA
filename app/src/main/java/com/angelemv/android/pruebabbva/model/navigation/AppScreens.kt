9b24a43101b1ebb553c24d618431ef25822e3f7f
Angel Morales <elianec13@gmail.com>
2024-12-21 17:41:12 -0600

    Nav Component and LoginScreen Added

---
+++ app/src/main/java/com/angelemv/android/pruebabbva/model/navigation/AppScreens.kt
@@ -0,0 +1,6 @@
+package com.angelemv.android.pruebabbva.model.navigation
+
+sealed class AppScreens (val route: String) {
+    object SplashScreen : AppScreens("splash_screen")
+    object LoginScreen : AppScreens("login_screen")
+}
---
+++ app/src/main/java/com/angelemv/android/pruebabbva/views/SplashScreen.kt
@@ -0,0 +1,65 @@
+package com.angelemv.android.pruebabbva.views
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.angelemv.android.pruebabbva.R
+import com.angelemv.android.pruebabbva.model.navigation.AppScreens
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(nav: NavHostController) {
+    LaunchedEffect(key1 = true) {
+        delay(3000)
+        nav.popBackStack()
+        nav.navigate(AppScreens.LoginScreen.route)
+    }
+    Splash()
+}
+
+
+@Composable
+fun Splash() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_splash),
+            contentDescription = "App Logo",
+            modifier = Modifier.size(180.dp)
+        )
+        Spacer(modifier = Modifier.height(46.dp))
+        Text(
+            " Prueba BBVA \n Morales Varela Angel Ernesto",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Thin,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(3.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    Splash()
+}

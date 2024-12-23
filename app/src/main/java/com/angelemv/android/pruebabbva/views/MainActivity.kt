ca771d60fafa73568082ae7f59388e7049e21cab
Angel Morales <elianec13@gmail.com>
2024-12-22 19:30:34 -0600

    Clean project

--- app/src/main/java/com/angelemv/android/pruebabbva/views/MainActivity.kt
+++ app/src/main/java/com/angelemv/android/pruebabbva/views/MainActivity.kt
@@ -3,18 +3,9 @@ package com.angelemv.android.pruebabbva.views
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.angelemv.android.pruebabbva.model.navigation.AppNavigation
-import com.angelemv.android.pruebabbva.ui.theme.PruebaBBVATheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
